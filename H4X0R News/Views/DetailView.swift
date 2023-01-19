7fafc94fc65ea17f8b37e055d727cec957bbc7f8
Mario Alves <marchetti2@gmail.com>
2023-01-18 23:10:57 -0300

    Up

--- H4X0R News/Views/DetailView.swift
+++ H4X0R News/Views/DetailView.swift
@@ -9,9 +9,7 @@
 import SwiftUI
 
 struct DetailView: View {
-    
     let url: String?
-    
     var body: some View {
         WebView(urlString: url)
     }
--- H4X0R News/Views/WebView.swift
+++ H4X0R News/Views/WebView.swift
@@ -26,5 +26,4 @@ struct WebView: UIViewRepresentable {
             }
         }
     }
-    
 }
